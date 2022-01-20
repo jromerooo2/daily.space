6fe80f9c0a68071e36b7c49940e9858a0b077c2e
Juan Romero <75763058+jromerooo2@users.noreply.github.com>
2022-01-19 20:01:06 -0600

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import NavBar from './components/NavBar.vue'
+import What from './components/Main/What.vue'
+import Footer from './components/Footer.vue'
+</script>
+
+<template>
+  <NavBar />
+    <What />
+  <Footer />
+</template>
+
---
+++ src/components/Footer.vue
@@ -0,0 +1,16 @@
+<template >
+    <footer class="flex items-center h-32 bg-black">
+        <div class="justify-between h-auto m-auto">
+            <ul class="flex justify-center text-center space-x-7">
+                <li class="text-gray-500 link">SPACEY © </li>
+                <a href="http://www.twitter.com/jromerooo2" target="_blank"><li class="text-white link hover:text-gray-500">TWITTER</li></a>
+                <a href="http://www.github.com/jromerooo2" target="_blank"><li class="text-white link hover:text-gray-500 ">GITHUB</li></a>
+            </ul>
+        </div>
+    </footer>
+</template>
+<script>
+export default {
+    name: "Footer",    
+}
+</script>
