6250806fc0c21db3fd3bc1ae1bd2f76b8ac2df47
Juan Romero <75763058+jromerooo2@users.noreply.github.com>
2022-02-09 12:08:27 -0600

    :unicorn: responsive iframe when video

--- src/components/apod/Apod.vue
+++ src/components/apod/Apod.vue
@@ -1,17 +1,26 @@
 <template id="apod" class="">
     <div class="flex flex-col p-5 space-y-10 bg-black">
-        <div class="block md:flex md:justify-center md:items-center">
+        <div class="flex flex-col-reverse md:flex-row md:justify-center md:items-center">
             <div class="flex-none space-y-4 md:w-1/2">
                 <h1 class="text-3xl font-bold text-white font-poppins">"{{apod.title}}"</h1>
-                <h1 class="font-bold text-white font-poppins">Author: {{apod.copyright}}</h1>
+                <h1 class="font-bold text-white font-poppins">Author: 
+                {{apod.copyright === "" ? 
+                        "Anonymus": apod.copyright
+                }}</h1>
                 <p class="text-justify text-white font-ibm">{{apod.explanation}}</p>
             </div>
-            <div class="grow md:w-1/2">
-                <iframe :src="apod.url" class="p-4 mx-auto" />
+            <div class="md:w-1/2 src_container">
+                <!-- if image -->                
+                <img v-if="media_type === img" :src="apod.url" class="p-4 mx-auto" />
+                
+                <!-- if video -->
+                <iframe v-else class="block w-full mx-auto h-96" :src="apod.url" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>  
+                          
             </div>
         </div>
     </div>
 </template>
+
 <script>
 import axios from 'axios';
 import { defineComponent, ref } from "vue";
@@ -20,6 +29,7 @@ export default defineComponent({
     name: "APOD",
     data(){
         return{
+            media_type: '',
             apod: {},
         }
     },
@@ -27,7 +37,9 @@ export default defineComponent({
         async getLaunches(){
             const response = await axios.get('https://api.nasa.gov/planetary/apod?api_key=VdRJDtCOwCkmwx4pnyXApD34Q94lQaCOXWJys97t');
             this.apod = await response.data;
+            this.media_type = this.apod.media_type;
             console.log(this.apod);
+            console.log(this.media_type);
             }
     },
     mounted() {
